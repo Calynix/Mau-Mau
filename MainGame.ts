b1bcda19a0cf17ba39164e290bd5f834c4efc02a
Calynix <undead.cat.8@gmail.com>
2025-04-07 13:26:37 +0200

    write mock code

---
+++ MainGame.ts
@@ -0,0 +1,46 @@
+//import crads interface
+
+class MauMauGame {
+    deckId: string;
+    currentCard: Card;
+    players: string[] = [];
+    playerHands: { [key: string]: Card[] } = {}; // cards specific to player
+
+    // Initialize the game: shuffle deck and prepare game
+
+    async initializeGame() {
+        // Shuffle deck (API call)
+        // Draw first card
+        // use await as async
+    }
+
+    async drawCard(): Promise<Card[]> {
+        // API call to draw cards
+    }
+
+    // Deal cards to players
+    async dealCards(numPlayers: number = 2, cardsPerPlayer: number = 5) {
+        // Draw cards and dstribute them
+    }
+
+    // Start the round: draw the first card
+    async startRound() {
+        const card = await this.drawCard();
+        this.currentCard = card[0];
+        console.log("First card on pile:", this.currentCard);
+    }
+
+    canPlayCard(card: Card): boolean {
+        if (!this.currentCard) return false;
+        return card.suit === this.currentCard.suit || card.value === this.currentCard.value;
+    }
+
+    playCard(player: string, card: Card) {
+       //play card
+    }
+
+
+    showStatus() {
+        //show Game Status
+    }
+}
